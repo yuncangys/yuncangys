6473c16ca12e5142aeef007a21216aa3574c8d95
yuncangys <709749987@qq.com>
2024-11-22 09:25:19 +0800

    Add files via upload

--- tx4k.js
+++ tx4k.js
@@ -158,7 +158,7 @@ var rule = {
         let zp = d.filter(function(it) {
             return !(it.type && it.type !== "正片")
         });
-        VOD.vod_play_from = yg.length < 1 ? "4K" : "$$$预告及花絮";
+        VOD.vod_play_from = yg.length < 1 ? "4KHDR" : "4KHDR$$$预告及花絮";
         VOD.vod_play_url = yg.length < 1 ? d.map(function(it) {
             return it.title + "$" + it.url
         }).join("#") : [zp, yg].map(function(it) {
